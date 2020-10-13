0326bb9bfbe3fde8c965310420bd4dfc328b9e59
Crowdbotics <team@crowdbotics.com>
2020-10-13 07:04:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chetna_mobile_test__13211/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chetna_mobile_test__13211;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "chetna_mobile_test__13211";
+    }
+}
